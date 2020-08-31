f4b008c5db2d988e64168ad2c9736408a02c63ec
Toye Ogundepo <ogundepo@gmail.com>
2020-08-31 01:10:21 -0500

    Updated form by adding appropriate type values, added onChange function, added state, bind, handleChange, and handleSubmit functions.

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,26 @@ import { Button, Col, Form, FormGroup, Label, Input, FormText, Row } from 'react
 class Contact extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            phoneNumber: "",
+            message: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+
+        const { firstName, lastName, email, phoneNumber, message } = this.state
     }
 
     render() {
@@ -23,39 +43,39 @@ class Contact extends React.Component {
                 <div className="container-fluid mt-5">
                 <div className="row">
                     <div className="col-sm-12 d-flex justify-content-center">
-                        <Form className="col-xs-6 col-sm-8 col-md-8 mb-3 form-style" model="feedbackForm">
+                        <Form onSubmit={this.handleSubmit} className="col-xs-6 col-sm-8 col-md-8 mb-3 form-style" model="feedbackForm">
                             <Row className="form-group">
                                 <Label htmlFor="firstName" md={2}>First</Label>
                                 <Col md={10}>
-                                    <Input style={{backgroundColor: "transparent", color: "white"}} className="input" type="text" name="firstName" placeholder="First Name" />
+                                    <Input style={{backgroundColor: "transparent", color: "white"}} className="input" type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
                                 </Col>
                             </Row>
 
                             <Row className="form-group">
                                 <Label htmlFor="lastName" md={2}>Last</Label>
                                 <Col md={10}>
-                                    <Input style={{backgroundColor: "transparent", color: "white"}} type="text" name="lastName" placeholder="Last Name" />
+                                    <Input style={{backgroundColor: "transparent", color: "white"}} type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
                                 </Col>
                             </Row>
                             
                             <Row className="form-group">
-                                <Label for="phoneNumber" md={2}>Phone</Label>
+                                <Label htmlFor="phoneNumber" md={2}>Phone</Label>
                                 <Col md={10}>
-                                    <Input style={{backgroundColor: "transparent", color: "white"}} type="text" name="phoneNumber" placeholder="Phone Number" />
+                                    <Input style={{backgroundColor: "transparent", color: "white"}} type="text" name="phoneNumber" placeholder="Phone Number" onChange={this.handleChange} />
                                 </Col>
                             </Row>
 
                             <Row className="form-group">
                                 <Label htmlFor="email" md={2}>E-Mail</Label>
                                 <Col md={10}>
-                                    <Input style={{backgroundColor: "transparent", color: "white"}} type="text" name="email" placeholder="E-Mail Address" />
+                                    <Input style={{backgroundColor: "transparent", color: "white"}} type="email" name="email" placeholder="E-Mail Address" onChange={this.handleChange} />
                                 </Col>
                             </Row>
 
                             <Row className="form-group">
                                 <Label for="message" md={2}>Message</Label>
                                 <Col md={10}>
-                                    <Input style={{backgroundColor: "transparent", color: "white"}} type="textarea" name="message" />
+                                    <Input style={{backgroundColor: "transparent", color: "white"}} type="textarea" name="message" onChange={this.handleChange} />
                                 </Col>
                             </Row>
                             
