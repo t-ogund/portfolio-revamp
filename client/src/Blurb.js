810995b3b535c9e2f5aadbaca2ce23d965a804f4
Toye Ogundepo <ogundepo@gmail.com>
2020-09-04 20:22:14 -0500

    Prevented typewriter overflow or wrap (media queries).

--- client/src/Blurb.js
+++ client/src/Blurb.js
@@ -5,7 +5,7 @@ function Blurb() {
         <div className="container">
             <div className="row">
                 <div className="col-lg-12">
-                    <blockquote className="blurb-text mb-5"><p className="blurb-paragraph typewriter">Hi, my name is Toye. I am a web developer.</p></blockquote>
+                    <blockquote className="blurb-text mb-5"><p className="blurb-paragraph typewriter">Hi, my name is Toye. <span class="intro">I am a web developer. </span></p></blockquote>
                 </div>
             </div>
         </div>
