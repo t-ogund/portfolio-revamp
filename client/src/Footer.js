bab0f5f83c31af3e58f40d545f9924d6ad8a4387
Toye Ogundepo <ogundepo@gmail.com>
2020-09-14 17:54:19 -0500

    modified social links

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -14,8 +14,8 @@ class Footer extends React.Component {
                     <div className="col">
                     </div>
                     <div className="col d-flex justify-content-center align-items-center mt-5">
-                        <a href="https://www.linkedin.com/" target="_blank"><i className="fab fa-linkedin mr-4"></i></a>
-                        <a href="https://github.com/" target="_blank"><i className="fab fa-github-square"></i></a>
+                        <a href="https://www.linkedin.com/in/olatoye-ogundepo-1bb416151/" target="_blank"><i className="fab fa-linkedin mr-4"></i></a>
+                        <a href="https://github.com/t-ogund" target="_blank"><i className="fab fa-github-square"></i></a>
                         {/* <a href="" target="_blank"><i className="fab fa-twitter-square ml-4"></i></a> */}
                     </div>
                     <div className="col d-flex justify-content-center align-items-center mt-5 back-to-top">
