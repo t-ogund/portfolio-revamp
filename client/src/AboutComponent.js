0d516c431f561be3014b786e56286b8f9c740cd5
Toye Ogundepo <ogundepo@gmail.com>
2020-11-24 10:41:48 -0600

    updated about component.

--- client/src/AboutComponent.js
+++ client/src/AboutComponent.js
@@ -7,8 +7,8 @@ function About(props) {
     return (
         <React.Fragment>
             <Navbar_Component />
-            <div className="container-fluid about-row">
-                <Row>
+            <div className="container-fluid about-row-container">
+                <Row className="about-row">
 
                     <Col className="about-text" md={6}>
                         <p>Hi, my name is Toye Ogundepo. I'm a web developer who likes bringing websites and applications to life. I enjoy taking on new challenges and finding solutions to complex problems. If you are in need of a website or web application, feel free to contact me.</p>
@@ -17,7 +17,15 @@ function About(props) {
                     </Col>
                 </Row>
             </div>
-            <Footer />
+            <div className="container-fluid">
+                <Row>
+                    {/* <Col> */}
+                        <Footer />
+                    {/* </Col> */}
+                </Row>
+            </div>
+            
+
         </React.Fragment>
     )
 }
